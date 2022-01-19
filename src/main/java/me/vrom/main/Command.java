734840ffe9ec2e9f260c0d16977a366c0d86c193
AarushKiller <67967009+AarushKiller@users.noreply.github.com>
2022-01-19 10:46:26 +0545

    Initial commit

---
+++ src/main/java/me/vrom/main/Command.java
@@ -0,0 +1,33 @@
+package me.vrom.main;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class Command implements CommandExecutor {
+
+
+    @Override
+    public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("tutorial")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                if (sender.hasPermission("tutorial1")) {
+                    ItemStack diamond = new ItemStack(Material.DIAMOND);
+                    ItemStack bricks = new ItemStack(Material.BRICK);
+                    bricks.setAmount(25);
+                    player.getInventory().addItem(bricks, diamond);
+                    player.sendMessage(ChatColor.GREEN + "You got some stuff!");
+                } else {
+                    player.sendMessage(ChatColor.RED + "JUST NO!");
+                }
+            }
+        }
+        return true;
+    }
+
+
+}
